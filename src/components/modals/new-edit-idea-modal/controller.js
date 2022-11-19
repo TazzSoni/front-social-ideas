fb2ab9afe6c62d4b6457074f559bcd9ecb21ca58
Schork <eduardo.schork1814@gmail.com>
2022-11-19 19:01:46 -0300

    fix: hide attach file icon when does not have attach

--- src/components/modals/new-edit-idea-modal/controller.js
+++ src/components/modals/new-edit-idea-modal/controller.js
@@ -41,7 +41,9 @@ export const useNewEditIdeaController = ({
     setIdeaStage(currentIdea?.stage);
     setIdeaTags(currentIdea?.tags);
 
-    setSelectedFile({ id: currentIdea?.fileId, name: currentIdea?.fileName });
+    if (currentIdea?.fileId && currentIdea?.fileName) {
+      setSelectedFile({ id: currentIdea?.fileId, name: currentIdea?.fileName });
+    }
   }, [currentIdea]);
 
   const onEditIdeaHandler = async () => {
@@ -55,7 +57,7 @@ export const useNewEditIdeaController = ({
     };
 
     if (selectedFile == null) {
-      data = { ...data, file: null }
+      data = { ...data, file: null };
     } else if (!selectedFile.id) {
       data = { ...data, file: selectedFile };
     }
@@ -78,7 +80,7 @@ export const useNewEditIdeaController = ({
       tags: ideaTags,
       file: selectedFile,
     };
-    debugger
+
     const { status } = await createNewIdea({ data });
 
     if (status == 201) {
